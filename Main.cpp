04a6f2896439dc4a8ab921d6f6d29195bb3e210b
Anton Dospekhov <tony.dospiekhov@gmail.com>
2024-05-08 03:55:16 +0300

    Clean up example

--- Main.cpp
+++ Main.cpp
@@ -1,37 +1,29 @@
-// Declares clang::SyntaxOnlyAction.
 #include "clang/Frontend/FrontendActions.h"
 #include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/Tooling/Tooling.h"
-// Declares llvm::cl::extrahelp.
 #include "llvm/Support/CommandLine.h"
-
-using namespace clang::tooling;
-using namespace llvm;
 
 // Apply a custom category to all command-line options so
 // that they are the only ones displayed.
-static llvm::cl::OptionCategory MyToolCategory("my-tool options");
+static llvm::cl::OptionCategory VisualizerSpecificCategory("Code Visualizer options");
 
 // CommonOptionsParser declares HelpMessage with a
 // description of the common command-line options related to
 // the compilation database and input files. It's nice to
 // have this help message in all tools.
-static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
-
-// A help message for this specific tool can be added
-// afterwards.
-static cl::extrahelp MoreHelp("\nMore help text...\n");
+static llvm::cl::extrahelp CommonHelp(clang::tooling::CommonOptionsParser::HelpMessage);
 
 int main(int argc, const char** argv)
 {
-	auto ExpectedParser = CommonOptionsParser::create(argc, argv, MyToolCategory);
-	if (!ExpectedParser)
+	using namespace clang::tooling;
+	llvm::Expected<CommonOptionsParser> commandLineParser = CommonOptionsParser::create(argc, argv, VisualizerSpecificCategory);
+
+	if (!commandLineParser)
 	{
-		// Fail gracefully for unsupported options.
-		llvm::errs() << ExpectedParser.takeError();
-		return 1;
+		llvm::errs() << commandLineParser.takeError();
+		return -1;
 	}
-	CommonOptionsParser& OptionsParser = ExpectedParser.get();
-	ClangTool Tool(OptionsParser.getCompilations(), OptionsParser.getSourcePathList());
+
+	ClangTool Tool(commandLineParser.get().getCompilations(), commandLineParser.get().getSourcePathList());
 	return Tool.run(newFrontendActionFactory<clang::SyntaxOnlyAction>().get());
 }
