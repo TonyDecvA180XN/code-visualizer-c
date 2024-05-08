b436ab7c2d0cbad044352697419c2b2ae3ee54c8
Anton Dospekhov <tony.dospiekhov@gmail.com>
2024-05-08 04:33:52 +0300

    Add CMakeRC library to include template files and add html response template

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,8 @@
 #include "clang/Tooling/Tooling.h"
 #include "llvm/Support/CommandLine.h"
 
+#include "cmrc/cmrc.hpp"
+
 // Apply a custom category to all command-line options so
 // that they are the only ones displayed.
 static llvm::cl::OptionCategory VisualizerSpecificCategory("Code Visualizer options");
@@ -12,6 +14,8 @@ static llvm::cl::OptionCategory VisualizerSpecificCategory("Code Visualizer opti
 // the compilation database and input files. It's nice to
 // have this help message in all tools.
 static llvm::cl::extrahelp CommonHelp(clang::tooling::CommonOptionsParser::HelpMessage);
+
+CMRC_DECLARE(VisualizerResources);
 
 int main(int argc, const char** argv)
 {
@@ -23,6 +27,10 @@ int main(int argc, const char** argv)
 		llvm::errs() << commandLineParser.takeError();
 		return -1;
 	}
+
+	cmrc::embedded_filesystem filesystem = cmrc::VisualizerResources::get_filesystem();
+	cmrc::file responsePage = filesystem.open("Templates/ResponsePage.html");
+	std::string content(responsePage.begin(), responsePage.end());
 
 	ClangTool Tool(commandLineParser.get().getCompilations(), commandLineParser.get().getSourcePathList());
 	return Tool.run(newFrontendActionFactory<clang::SyntaxOnlyAction>().get());
